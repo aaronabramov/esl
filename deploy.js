dbb0d8730c96a241737dd60ebdfa3f2c1dde12d7
Dmitrii Abramov <dabramov@yahoo-inc.com>
2014-08-30 12:06:54 -0700

    build

--- deploy.js
+++ deploy.js
@@ -3,8 +3,10 @@ var Kirov = require('kirov'),
 
 k.username('dmitriiabramov');
 k.host('rheia.us');
+k.exec('rm -rf esl');
 k.exec('mkdir esl');
 k.exec('git clone https://github.com/dmitriiabramov/esl.git esl');
 k.exec('cd esl && npm install');
+k.exec('cd esl && make build');
 k.exec('cd esl && ./node_modules/forever/bin/forever start run.js');
 
