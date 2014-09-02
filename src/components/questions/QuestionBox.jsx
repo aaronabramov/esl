93bd4e610cdd7fff235f955972fbf91404d06d94
scsper <scsper@gmail.com>
2014-09-01 17:52:46 -0700

    add a basic explanation for a question

--- src/components/questions/QuestionBox.jsx
+++ src/components/questions/QuestionBox.jsx
@@ -70,6 +70,8 @@ var QuestionBox = React.createClass({
 
                 <div className="question-footer">
                     <div className="question-next">
+                        <div className="question-explanation">You need to use the interrogative form.</div>
+
                         <button className="question-next-button">
                             <span className="glyphicon glyphicon-chevron-right"></span>
                             <span className="next-text">Next</span>
