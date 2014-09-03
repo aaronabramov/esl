ed585b285bf76cab4a4a0fd594f3b63123c75354
Dmitrii Abramov <dabramov@yahoo-inc.com>
2014-09-02 22:44:29 -0700

    app => commonjs

--- src/application.js
+++ src/application.js
@@ -1,3 +1,6 @@
-QuestionsActionCreator.initialize();
+var React = require('react'),
+    App = require('./components/app.jsx');
+
+require('./actions/questions_action_creator.js').initialize();
 
 React.renderComponent(App(null), document.body);
