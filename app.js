b9888bc4187a2fec9a12a02c585e287dde9e2985
scsper <scsper@gmail.com>
2014-09-22 10:03:51 -0700

    first cut at adding sessions

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     passport = require('passport'),
     FacebookStrategy = require('passport-facebook').Strategy,
+
     app = express();
 
 require('node-jsx').install({
@@ -16,6 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,6 +30,21 @@ passport.use(new FacebookStrategy({
         done(null, {'user': profile});
     })
 );
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Facebook profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 // Redirect the user to Facebook for authentication.  When complete,
 // Facebook will redirect the user back to the application at
