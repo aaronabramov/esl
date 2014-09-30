4832d7d4677e6e6d7e0ca2769602d84f3c5fb55b
scsper <scsper@gmail.com>
2014-09-29 21:38:11 -0700

    add quiz results model

---
+++ models/quiz_results.js
@@ -0,0 +1,10 @@
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define('quiz_results', {
+        quiz_id: {
+            type: DataTypes.INTEGER
+        },
+        total_correct: {
+            type: DataTypes.INTEGER
+        }
+    });
+};
