aa410deb3ccb2deccd211543ecbfad05cbbe8993
Dmitrii Abramov <dabramov@yahoo-inc.com>
2014-10-07 21:52:49 -0700

    find or create user

--- middleware/strategies/facebook.js
+++ middleware/strategies/facebook.js
@@ -8,15 +8,23 @@ module.exports = function() {
         clientSecret: config.get('facebook.clientSecret'),
         callbackURL: "http://local.esl.com:3009/login/facebook/callback"
     }, function(accessToken, refreshToken, profile, done) {
+        console.log(profile);
         var id = profile.id,
-            name = profile.name;
+            displayName = profile.displayName;
 
-        models.users.create({
+        models.users.findOrCreate({
             facebook_id: id
-        }).complete(function(err, user) {
-            done(null, {
-                'user': profile
+        }).success(function(user) {
+            user.display_name = displayName;
+            user.save().success(function() {
+                done(null, {
+                    user: profile
+                });
+            }).error(function(err) {
+                done(err);
             });
+        }).error(function(err) {
+            done(err);
         });
     });
 };
--- models/user.js
+++ models/user.js
@@ -2,6 +2,9 @@ module.exports = function(sequelize, DataTypes) {
     return sequelize.define('users', {
         facebook_id: {
             type: DataTypes.STRING
+        },
+        display_name: {
+            type: DataTypes.STRING
         }
     });
 };
