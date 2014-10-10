4bb2225b7d7167f26d079067ef2426ec02ed0e00
scsper <scsper@gmail.com>
2014-10-09 21:20:23 -0700

    basic video prototype with video.js

--- s3_test.js
+++ s3_test.js
@@ -1,17 +1,17 @@
 AWS = require('aws-sdk'),
-    config = require('config');
+config = require('config');
 
 AWS.config.accessKeyId = config.get('aws.access_key_id');
 AWS.config.secretAccessKey = config.get('aws.secret_access_key');
 AWS.config.region = 'us-west-1';
 
-var s3 = new AWS.S3()
+var s3 = new AWS.S3();
 
 // s3.listBuckets(function(err, data) {
 //     console.log(err, data);
 // });
 
-var OBJ_KEY = 'Beginner 1_4.m4v';
+var OBJ_KEY = 'beginner_1_4.mp4';
 
 // s3.listObjects({Bucket: 'esl-videos-alice'}, function(err, data) {
 //     console.log(err, data);
