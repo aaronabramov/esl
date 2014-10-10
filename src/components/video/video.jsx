2248a5fb5d3a4143358fd4a757e9a2eb66983202
scsper <scsper@gmail.com>
2014-10-10 00:34:43 -0700

    display the video component on the page.
    the video is set through dangerouslySetInnerHTML because the error 'Invariant Violation: ReactMount: Two valid but unequal nodes with the same `data-reactid`' keeps showing up when using React to render the video.  My guess is that the library, video.js, is doing some DOM manipulation on the video on mouse over.  Since the DOM is being manipulated, React doesn't like it.  Setting the inner HTML will take the element outside of React, which leaves it safe for a 3rd party to manipulate.
    
    The video component is ugly right now, too.  Styling will come later.

--- src/components/video/video.jsx
+++ src/components/video/video.jsx
@@ -3,11 +3,14 @@
 var React = require('react');
 
 var Video = React.createClass({
+
     render: function() {
+        var link = 'https://esl-videos-alice.s3-us-west-1.amazonaws.com/beginner_1_4.mp4?AWSAccessKeyId=AKIAJ2ZJDCY6PIMCYNAQ&Expires=1412926234&Signature=lFtei3NLG6nk4914hg%2BuAUuvoBg%3D',
+            html = '<video id="video" class="video-js vjs-default-skin"' +
+                   ' controls preload="auto" width="640" height="264" data-setup=\'{"example_option":true}\'>' +
+                   ' <source src=' + link + ' type="video/mp4" /></video>';
         return (
-            <video id="video" className="video-js vjs-default-skin" controls preload="auto" width="640" height="264" data-setup='{"example_option":true}'>
-                 <source src="https://esl-videos-alice.s3-us-west-1.amazonaws.com/beginner_1_4.mp4?AWSAccessKeyId=AKIAJ2ZJDCY6PIMCYNAQ&Expires=1412917941&Signature=t3qKsUyLyQoNf2PPFKtwlwp7z24%3D" type='video/mp4' />
-            </video>
+            <div dangerouslySetInnerHTML={{__html: html}}></div>
         );
     }
 });
