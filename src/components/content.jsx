8146afc693abda98b2582f7a3a3afe2a80cc48bd
scsper <scsper@gmail.com>
2014-10-10 23:03:26 -0700

    remove video component

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -76,7 +76,6 @@ module.exports = React.createClass({
 
         return (
             <div className="content pure-u-1 pure-u-md-3-4">
-                <Video />
                 {component}
             </div>
         );
