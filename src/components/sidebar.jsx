cad27f853c4b91eee9f2a22f8d296c95e1c0abbe
scsper <scsper@gmail.com>
2014-10-12 16:40:32 -0700

    more basic layout

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -9,6 +9,10 @@ module.exports = React.createClass({
                 <div className="header">
                     <h1 className="brand-title">ESL</h1>
                     <h2 className="lesson">Being Verbs</h2>
+                    <h3 className="lesson-piece">Video</h3>
+                    <h3 className="lesson-piece">Quiz</h3>
+
+
                 </div>
             </div>
         );
