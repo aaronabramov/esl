f6e81c71fe83f203028e41ea4e57fcf9ecfa6b4a
scsper <scsper@gmail.com>
2014-10-23 13:41:09 -0700

    get rid of unneeded quiz code

--- src/browser/actions/quiz_action_creator.js
+++ src/browser/actions/quiz_action_creator.js
@@ -3,20 +3,6 @@ var QuizService = require('../services/quiz_service.js'),
     Dispatcher = require('../dispatcher/dispatcher.js');
 
 module.exports = {
-    initialize: function() {
-        QuizService.getQuestions().then(function(data) {
-            Dispatcher.handleServerAction({
-                actionType: QuizConstants.INITIALIZE,
-                questions: data.questions
-            });
-        }, function(error) {
-            Dispatcher.handleServerAction({
-                actionType: QuizConstants.ERROR,
-                error: error
-            });
-        });
-    },
-
     submitAnswer: function(submittedAnswer) {
         Dispatcher.handleViewAction({
             actionType: QuizConstants.SUBMIT_ANSWER,
