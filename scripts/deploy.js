1ab3f5853aeed8de9ca323656862b25be4ea5fc2
Dmitrii Abramov <dabramov@yahoo-inc.com>
2014-11-24 20:35:24 -0800

    more magic

---
+++ scripts/deploy.js
@@ -0,0 +1,27 @@
+var SSH = require('ssh-kit'),
+    ssh = new SSH();
+
+
+// open stack host and username
+ssh.set('username', 'dmitriiabramov');
+ssh.set('host', 'turboenglish.net');
+ssh.set('sshKey', '~/.ssh/dmitrii');
+
+// create dir if it does not exist yet
+ssh.exec('mkdir -p ~/esl_deploy_tmp');
+
+// clone master branch of the repo
+ssh.exec('git clone git@github.com:dmitriiabramov/esl.git ~/esl_deploy_tmp');
+
+// install packages
+ssh.exec('cd ~/esl_deploy_tmp && npm install');
+
+// if it exists and was deployed before then try to stop all forever daemons
+ssh.exec('cd ~/esl_deploy_tmp && [ -f ./node_modules/forever/bin/forever ] && ./node_modules/forever/bin/forever stopall');
+
+// remove previously deployed directory and rename current tmp
+ssh.exec('rm -rf ~/esl');
+ssh.exec('mv ~/esl_deploy_tmp ~/esl');
+
+// and daemonize the process redirectiong STDOUT of the server to ~/out.log
+ssh.exec('./esl/node_modules/forever/bin/forever start -o ~/esl_out.log -e ~/esl_err.log ~/esl/run.js');
