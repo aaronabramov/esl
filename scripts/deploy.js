d4cc6c0fadad01a8f8c2c7870c77ca0ff7c9e177
Dmitrii Abramov <dabramov@yahoo-inc.com>
2014-11-24 21:54:49 -0800

    run from CWD

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -29,4 +29,4 @@ ssh.exec('rm -rf ~/esl/config && ln -s ~/esl_config ~/esl/config');
 ssh.exec('cd ~/esl/ && ./node_modules/grunt-cli/bin/grunt browserify');
 
 // and daemonize the process redirectiong STDOUT of the server to ~/out.log
-ssh.exec('NODE_ENV=production ./esl/node_modules/forever/bin/forever start -o ~/esl_out.log -e ~/esl_err.log ~/esl/run.js');
+ssh.exec('NODE_ENV=production cd ~/esl && ./node_modules/forever/bin/forever start -o ~/esl_out.log -e ~/esl_err.log ~/esl/run.js');
