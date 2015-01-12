622b9b018cb72730fa5a8edb9db69b6bc293fb96
Dmitrii Abramov <dabramov@yahoo-inc.com>
2015-01-11 21:10:59 -0800

    add prod user auth

--- src/server/middleware/authorize.js
+++ src/server/middleware/authorize.js
@@ -2,11 +2,17 @@
  * Middleware that checks if user id is in the list of enabled users. If not
  * then it will throuw 'not authorized' error which prevents not authorized
  * users from accessing the course.
+ *
+ * FB user id api key dependent. Same user will have different id in
+ * dev and production.
  */
 
-ENABLED_USERS = [
-    '10204961219574652'
-];
+ENABLED_USERS_DEV = [
+        '10204961219574652', // Dmitrii Abramov
+    ],
+    ENABLED_USERS = ENABLED_USERS_DEV.concat([
+        '10205312732642259' // Dmitrii Abramov
+    ]);
 
 module.exports = function(req, res, next) {
     console.log(~ENABLED_USERS.indexOf(req.user.facebook_id.toString()))
